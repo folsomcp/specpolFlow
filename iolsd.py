5f22e4054ba89be17f18b53482972dd44ae136b8
veropetit <83323003+veropetit@users.noreply.github.com>
2022-07-10 12:08:08 -0400

    Update iolsd.py

--- iolsd.py
+++ iolsd.py
@@ -483,7 +483,7 @@ class observation:
                 f.write('*** Spectrum of')
             else:
                 f.write(self.header)
-            f.write('  {} 5'.format(int(self.wl.size))
+            f.write('  {} 5'.format(int(self.wl.size)))
             for i in range(0,self.wl.size):
                 f.write('{:10.4f} {:11.4e} {:11.4e} {:11.4e} {:11.4e} {:11.4e}\n'.format(
                             self[i].wl, self[i].specI, self[i].specV,
