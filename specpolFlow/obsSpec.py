e0b545790bc5523b21aa60352d46e3a29a43cc80
Colin Folsom <folsomcp@gmail.com>
2024-04-20 20:30:54 -0400

    adding functions for splitting and merging orders

--- specpolFlow/obsSpec.py
+++ specpolFlow/obsSpec.py
@@ -4,6 +4,7 @@ Tools for manipulating spectra, typically spectropolarimetric observations.
 """
 
 import numpy as np
+import copy
 import specpolFlow as pol
 
 ###################################
@@ -22,7 +23,7 @@ class Spectrum:
     * specSig - the formal uncertainties, which apply to the other spectra
     """
 
-    def __init__(self,wl, specI, specV, specN1, specN2, specSig, header=None):
+    def __init__(self, wl, specI, specV, specN1, specN2, specSig, header=None):
         
         self.header=header
         self.wl = wl
@@ -71,12 +72,98 @@ class Spectrum:
     def __len__(self):
         return len(self.wl)
 
+    def concatenate(self, *args):
+        """
+        Combine this Spectrum with other Spectrum objects, passed as arguments,
+        concatenating them in order, into a new Spectrum object.
+        
+        :rtype: Spectrum
+        """
+        cat_header = self.header
+        cat_wl = self.wl
+        cat_specI = self.specI
+        cat_specV = self.specV
+        cat_specN1 = self.specN1
+        cat_specN2 = self.specN2
+        cat_specSig = self.specSig
+        for arg in args:
+            if not isinstance(arg, Spectrum):
+                raise ValueError('concatenate can only use Spectrum objects!')
+            cat_wl = np.concatenate((cat_wl, arg.wl))
+            cat_specI = np.concatenate((cat_specI, arg.specI))
+            cat_specV = np.concatenate((cat_specV, arg.specV))
+            cat_specN1 = np.concatenate((cat_specN1, arg.specN1))
+            cat_specN2 = np.concatenate((cat_specN2, arg.specN2))
+            cat_specSig = np.concatenate((cat_specSig, arg.specSig))
+        cat_spec = Spectrum(cat_wl, cat_specI, cat_specV, cat_specN1,
+                            cat_specN2, cat_specSig, header=cat_header)
+        return cat_spec
+
+    def coadd(self, *args):
+        """
+        coadd this spectrum with other spectra
+
+        Uses the the wavelength grid of this spectrum for the coadded spectrum.
+        Other spectra are interpolated onto this spectrum's wavelengths before
+        coadding.  Coadding here essentially averages spectra weighted
+        by 1/sigma**2  This assumes the spectra are continuum normalized,
+        and have reliable uncertainties.
+
+        Warning: regions with order overlap, or where wavelength goes backwards,
+        will produce incorrect results with this routine.  We strongly
+        recommend using the merge_orders function before using this routine.
+        
+        :rtype: Spectrum
+        """
+        #Set up the weighted average using self as the first entry
+        #weight by 1/sigma**2, and save the sum of the weights
+        spec = copy.deepcopy(self) #work on a copy (not self!)
+        weight = 1./self.specSig**2
+        spec.specI = self.specI*weight
+        spec.specV = self.specV*weight
+        spec.specN1 = self.specN1*weight
+        spec.specN2 = self.specN2*weight
+        totalWeight = weight.copy()
+        
+        for arg in args:
+            if not isinstance(arg, Spectrum):
+                raise ValueError('coadd can only use Spectrum objects!')
+            if np.any(arg.specSig <= 0.0):
+                raise ValueError("coadd requires uncertainties "
+                                 "(>0) for all points!")
+            if not np.all(np.diff(arg.wl) > 0):
+                print('Warning: in Spectrum.coadd, coadding spectra with order'
+                      ' overlap or decreasing wavelength.  This will cause'
+                      ' incorrect results in those regions.')
+            #Interpolate this spectrum onto the reference wavelengths
+            bspecI   = np.interp(spec.wl, arg.wl, arg.specI)
+            bspecV   = np.interp(spec.wl, arg.wl, arg.specV)
+            bspecN1  = np.interp(spec.wl, arg.wl, arg.specN1)
+            bspecN2  = np.interp(spec.wl, arg.wl, arg.specN2)
+            bspecSig = np.interp(spec.wl, arg.wl, arg.specSig)
+
+            #Add this spectrum to the weighted sums
+            weight = 1./bspecSig**2
+            spec.specI += bspecI*weight
+            spec.specV += bspecV*weight
+            spec.specN1 += bspecN1*weight
+            spec.specN2 += bspecN2*weight
+            totalWeight += weight
+
+        #Once all spectra have been added, complete the weighted average
+        spec.specI /= totalWeight
+        spec.specV /= totalWeight
+        spec.specN1 /= totalWeight
+        spec.specN2 /= totalWeight
+        spec.specSig = np.sqrt(1/totalWeight)
+        return spec
+            
     def IndividualLine(self, lambda0, lwidth):
         '''Select an individual line in the spectrum
 
         :param lambda0: wavelength of the transition in nanometers
         :param lwidth: distance from the line center for the determination
-                       of the wavelegnth window considered in line profile.
+                       of the wavelength window considered in line profile.
                        One element = same on each side of line center.
                        Two elements, left and right of line center.
         '''
@@ -97,7 +184,7 @@ class Spectrum:
             p_lwidth = [lambda0-lwidth, lambda0+lwidth]
             emission_line = self[np.logical_and(self.wl >= p_lwidth[0], self.wl <= p_lwidth[1])]
 
-        # Now we convert wavelenths to velocity space
+        # Now we convert wavelengths to velocity space
         c = 299792.458  #speed of light in km/s
         vel = c*(emission_line.wl-lambda0)/lambda0
 
@@ -124,7 +211,7 @@ class Spectrum:
                 ncols = 1
         
         with open(fname, 'w') as f:
-            #Optionaly write 2 lines of header            
+            #Optionally write 2 lines of header            
             if saveHeader:
                 if self.header is None:
                     f.write('*** Spectrum of\n')
@@ -148,6 +235,154 @@ class Spectrum:
                     f.write('{:10.4f} {:11.4e}\n'.format(
                         self.wl[i], self.specI[i]))
         return
+
+    def get_orders(self, ignoreGaps=False):
+        """
+        Split an observed echelle spectrum into individual echelle orders.
+        
+        This splits a concatenated spectrum into echelle orders, based on overlap
+        (places where wavelength decreases), and optionally gaps in wavelength
+        between orders.
+
+        :param ignoreGaps: if True then regions separated by gaps in wavelength
+                           are treated as one order, otherwise gaps are used as
+                           order edges
+        :return: a list of Spectrum objects, one for each identified order
+        """
+        #Define spectral order edges by a step backwards in wavelength (velocity)
+        #or a step forward in velocity more than 10x the average velocity step size.
+        #(use velocity rather than wavelength since pixel size in velocity is
+        # more consistent across a spectrum)
+        c = 299792.458  #speed of light in km/s
+        gapSize = 20.
+        velSteps = (self.wl[1:-1] - self.wl[0:-2])/self.wl[1:-1]*c
+        meanVelStep = np.mean(velSteps)
+        if ignoreGaps:
+            orderEdges = velSteps < 0.
+        else:
+            orderEdges = np.logical_or(velSteps < 0., velSteps > gapSize*meanVelStep)
+        indOrderEdges =  np.nonzero(orderEdges)[0] #last point in a spectral order
+        numOrders = indOrderEdges.shape[0] + 1
+
+        #Slice the orders out of this spectrum and put them in a new list
+        scp = copy.deepcopy(self) #start with a copy (don't just modify self!)
+        orders = []
+        indLast = 0
+        for i in range(numOrders - 1):
+            orders.append(scp[indLast : indOrderEdges[i]+1])
+            indLast = indOrderEdges[i]+1
+        orders.append(scp[indLast :])
+        
+        return orders
+
+    def get_orders_in_range(self, wl1, wl2, ignoreGaps=False):
+        """
+        Split an observed echelle spectrum into individual orders,
+        and return orders that include the specified wavelength range.
+
+        This splits a concatenated spectrum into echelle orders, based on overlap
+        (places where wavelength decreases), and optionally gaps in wavelength
+        between orders.  Returning the order, or orders, that include any part
+        of the specified wavelength range (wl1 and wl2 can be identical).
+
+        :param wl1: the start of the wavelength range to include
+        :param wl2: the end of the wavelength range to include
+        :param ignoreGaps: if True then regions separated by gaps in wavelength
+                           are treated as one order, otherwise gaps are used as
+                           order edges
+        :return: a list of Spectrum objects, one for each relevant order
+        """
+        if wl1 > wl2:
+            raise ValueError('Start wavelength must be smaller than end wavelength'
+                             ' got {:} {:}'.format(wl1, wl2))
+        
+        allOrders = self.get_orders(ignoreGaps)
+        retOrders = []
+        for order in allOrders:
+            #if the order starts before the desired range ends,
+            #and the order ends after the desired range starts,
+            #then some (or all) of it should be in range
+            if(wl2 > order.wl[0] and wl1 < order.wl[-1]): 
+                retOrders.append(order)
+
+        return retOrders
+
+    def merge_orders(self, mode='trim', midpoint=0.5):
+        """
+        Simple merging of spectral orders, for echelle spectra
+
+        The default mode 'trim' simply uses one order up to the midpoint
+        of an overlap region, and then uses the next order past the midpoint.
+        This is more robust against continuum normalization errors but
+        doesn't optimize the total signal-to-noise.
+        
+        The mode 'coadd' attempts to coadd orders in the overlap region.
+        It interpolates the second order onto the wavelength grid of the
+        first order, then averages the spectra weighted by 1/sigma**2.
+        This optimizes the total signal-to-noise but is vulnerable 
+        to continuum normalization errors at the edges of orders.
+        This mode requires reliable uncertainties.
+
+        :param mode: choice of 'trim', 'coadd'
+        :param midpoint: for mode 'trim', the fraction of the way through
+                         an overlap region treated as the midpoint
+        :rtype: Spectrum
+        """
+
+        orders = self.get_orders(ignoreGaps=True)
+        numOrders = len(orders)
+
+        if mode.lower() == 'trim':
+            #Merge by splitting orders at the midpoint of their overlap
+            wlStartMid = 0.0
+            for i in range(numOrders):
+                #find the midpoint of the overlap between this and the next order
+                if i == numOrders - 1: #for the last order include to the end
+                    wlEndMid = orders[i].wl[-1]
+                else:
+                    wlEndMid = (orders[i].wl[-1]*midpoint
+                                + orders[i+1].wl[0]*(1. - midpoint))
+                #use this order between this midpoint and the previous midpoint
+                induse = (orders[i].wl > wlStartMid) & (orders[i].wl <= wlEndMid)
+
+                #concatenate the used parts of the orders
+                if i == 0: #(or just use this order if it's the 1st)
+                    specM = orders[i][induse]
+                else:
+                    specM = specM.concatenate(orders[i][induse])
+                wlStartMid = wlEndMid
+            
+        elif mode.lower() == 'coadd':
+            #Merge by coadding orders inside the overlap region
+            if np.any(self.specSig <= 0.0):
+                raise ValueError("merge_orders in mode 'coadd' requires uncertainties "
+                                 "(>0) for all points!")
+            for i in range(numOrders):
+                #use this order's wavelength grid in the overlap with the next order
+                #(assuming all wavelength solutions are equally good)
+                if i < numOrders - 1:
+                    #identify the overlap range between orders
+                    ord1 = orders[i]
+                    ord2 = orders[i+1]
+                    indO1 = ord1.wl >= ord2.wl[0]
+                    indO2 = ord2.wl <= ord1.wl[-1]
+                    #Include one extra point from the next order for interpolation
+                    indO2[np.nonzero(np.logical_not(indO2))[0][0]] = True
+                    
+                    #coadd the region in the overlap
+                    ord1[indO1] = ord1[indO1].coadd(ord2[indO2])
+                    
+                #Save this order with coadded values,
+                #except for the overlap with the previous order
+                if i == 0: #(just use this order if it's the 1st)
+                    specM = orders[i]
+                else:
+                    induse = orders[i].wl > orders[i-1].wl[-1]
+                    specM = specM.concatenate(orders[i][induse])            
+        else:
+            raise ValueError("in merge_orders unrecognized mode '{:}'!".format(mode))
+        return specM
+    
 
 def read_spectrum(fname, trimBadPix=False, sortByWavelength=False):
     """
@@ -202,7 +437,7 @@ def read_spectrum(fname, trimBadPix=False, sortByWavelength=False):
         nHeader = 2
 
     #Get the number of lines of data in the file
-    nLines = 3 - nHeader #we've alread read 3 lines
+    nLines = 3 - nHeader #we've already read 3 lines
     for line in fObs:
         words = line.split()
         if len(words) == ncolumns:
